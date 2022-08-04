664af1b77de12fd37e3a444d5831ea6d02c07196
Arda AY <arda.ay@outlook.com.tr>
2022-08-04 23:21:25 +0300

    Introduction to C#
    Some examples are completed

---
+++ 03_HelloWorld/03_HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03_HelloWorld
+{
+    class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first C# project!");
+            
+            // Running this project from .exe will automatically closes the console
+            // with read code below, it waits to click enter before closing itself
+            Console.Read();
+
+            //Note: If this code is executed in MAC, terminal adds
+            //"Press any key to continue..." without Read command
+        }
+    }
+}

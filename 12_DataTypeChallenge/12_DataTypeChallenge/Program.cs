7b274af9322cf131039f9db35839e8ad644fb27e
Arda AY <arda.ay@outlook.com.tr>
2022-08-09 11:08:18 +0300

    DataTypes and Constants Examples

---
+++ 12_DataTypeChallenge/12_DataTypeChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _12_DataTypeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Types
+            // https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2008/ms228360(v=vs.90)?redirectedfrom=MSDN
+
+            byte myByte = 25;
+            Console.WriteLine(myByte);
+
+            sbyte mySbyte = -15;
+            Console.WriteLine(mySbyte);
+
+            int myInt = -12318925;
+            Console.WriteLine(myInt);
+
+            uint myUint = 123091;
+            Console.WriteLine(myUint);
+
+            short myShort = -32768;
+            Console.WriteLine(myShort);
+
+            ushort myUShort = 65535;
+            Console.WriteLine(myUShort);
+
+            float myFloat = -31.1289f;
+            Console.WriteLine(myFloat);
+
+            double myDouble = -12.1231250;
+            Console.WriteLine(myDouble);
+
+            char myCharacter = 'A';
+            Console.WriteLine(myCharacter);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            string myText = "I control text";
+            Console.WriteLine(myText);
+
+            string numText = "15";
+            int myNumFromText = int.Parse(numText);
+            Console.WriteLine(myNumFromText);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 13_Constants/13_Constants/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _13_Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constants as fields
+            const double PI = 3.14159265359;
+            const int weeks = 52, months = 12;
+
+            // Create a constant of type string with your birthday as its value
+            const string birthday = "21.05.1994";
+            const string birthday2 = "05.21.1994";
+            const string birthday3 = "2094-05-21";
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("My birthday is always going to be: {0}", birthday);
+                Console.ReadKey();
+            }
+        }
+    }
+}

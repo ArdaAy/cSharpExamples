cc50770b7d979d619a0d2595b1ced89da939e2ae
Arda AY <arda.ay@outlook.com.tr>
2022-08-13 02:20:58 +0300

    New Examples

---
+++ 21_SwitchCases/21_SwitchCases/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _21_SwitchCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 28;
+
+            switch (age)
+            {
+                case 15:
+                    Console.WriteLine("Too young to party in the club!");
+                    break;
+                case 28:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you then?");
+                    break;
+            }
+
+            if (age == 15)
+            {
+                Console.WriteLine("Too young to party in the club!");
+            }
+            else if (age == 28)
+            {
+                Console.WriteLine("Good to go!");
+            }
+            else
+            {
+                Console.WriteLine("How old are you then?");
+            }
+
+            string username = "Cengo";
+
+            switch (username)
+            {
+                case "Denis":
+                    Console.WriteLine("username is Cengo");
+                    break;
+                case "root":
+                    Console.WriteLine("username is root");
+                    break;
+                default:
+                    Console.WriteLine("username is unknown");
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}

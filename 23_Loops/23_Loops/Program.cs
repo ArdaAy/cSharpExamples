d223585a102816ba400efa38892365170715f1cb
Arda AY <arda.ay@outlook.com.tr>
2022-08-13 19:25:07 +0300

    Loops

---
+++ 23_Loops/23_Loops/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _23_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop example
+
+            for (int cnt = 1; cnt < 20; cnt += 2)
+            {
+                Console.WriteLine(cnt);
+            }
+            Console.Read();
+
+            // Do - While Loop Example
+
+            int lenghtOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLenght = nameOfAFriend.Length;
+                lenghtOfText += currentLenght;
+                wholeText += nameOfAFriend;
+            } while (lenghtOfText < 20);
+            Console.WriteLine("Thanks, that was enough! " + wholeText);
+            Console.Read();
+
+            // While Loop Example
+
+            int counter = 0;
+            string enteredText = "";
+            while (enteredText.Equals(""))
+            {
+                Console.WriteLine("Please press enter to increase amount by one and anything else" +
+                    " + enter if you want to finish counting");
+                enteredText = Console.ReadLine();
+
+                counter++;
+                Console.WriteLine("Current people count is {0}", counter);
+
+            }
+            Console.WriteLine("{0} people are inside the bus. Press enter to close the program", counter);
+            Console.Read();
+
+            // Continue and Break Example
+
+            for (int cnt = 0; cnt < 20; cnt++)
+            {
+
+                if (cnt % 2 == 0)
+                {
+                    Console.WriteLine("Now comes an odd number!");
+                    continue;
+                }
+                Console.WriteLine(cnt);
+                if(cnt == 10)
+                {
+                    Console.WriteLine("We need to end here.");
+                    break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 24_LoopChallenge/24_LoopChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _24_LoopChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalScore = 0;
+            int scoreCnt = 0;
+            
+            do
+            {
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine("Enter a new score (must be between 0 - 20), \nCurrently count: {0}, Currently Total: {1}", scoreCnt, totalScore);
+                int score = -1;
+                bool isParsed = int.TryParse(Console.ReadLine(), out score);
+                if (isParsed)
+                {
+                    if(score == -1)
+                    {
+                        break;
+                    }
+                    else if(score >= 0 && score <= 20)
+                    {
+                        totalScore += score;
+                        scoreCnt++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You typed wrong score. Try again.");
+                }
+
+            } while (true);
+
+            double average = (double)totalScore / (double)scoreCnt;
+            Console.WriteLine("Currently count: {0}, Currently Total: {1}", scoreCnt, totalScore);
+            Console.WriteLine("Average: "+ average);
+        }
+    }
+}

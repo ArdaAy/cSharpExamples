d6dab08b998540d77ead492fa812fa5ac60ebbbe
Arda AY <arda.ay@outlook.com.tr>
2022-08-13 20:32:55 +0300

    OOP Examples

---
+++ 25_OOPExample/25_OOPExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _25_OOPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human dolma = new Human(28, "Dolma", "Molma", "green");
+            Human cengo = new Human(1, "Cengo", "Mengo", "blue");
+            Human ciguli = new Human(45, "Ciguli", "blue");
+            dolma.IntroduceOnceself();
+            cengo.IntroduceOnceself();
+            ciguli.IntroduceOnceself();
+            Console.Read();
+        }
+    }
+}
---
+++ 26_OOPExample2/26_OOPExample2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _26_OOPExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human arda = new Human("Arda", "Ay", "brown", 28);
+            arda.IntroduceMyself();
+
+            Human julide = new Human("Jülide", "Ay", "blue");
+            julide.IntroduceMyself();
+
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+            Human cengiz = new Human("Cengiz");
+            cengiz.IntroduceMyself();
+
+            Human michael = new Human("Michael", "Jackson");
+            michael.IntroduceMyself();
+
+            Human franco = new Human("Franco", "Lorenz", 25);
+            franco.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}

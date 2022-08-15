f57973df2a862b7602b4c3750b41f469b83e248f
Arda AY <arda.ay@outlook.com.tr>
2022-08-15 22:14:56 +0300

    Array Examples

---
+++ 27_Arrays/27_Arrays/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _27_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare and initialize array grades
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            string input = Console.ReadLine();
+            // assign value to array grades at index 0
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+
+            // another way of initializing an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
+
+            // thrid way of initializing an array
+            int[] gradesOfMathStudentsB = new int[] { 15, 20, 3, 17, 18, 15 };
+
+            Console.WriteLine("Length of gradesOfMathStudentsA: {0}", gradesOfMathStudentsA.Length);
+            Console.ReadKey();
+
+            // Arrays with For Loop
+            Console.WriteLine("");
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i + 10;
+            }
+            Console.WriteLine("");
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element{0} = {1}", j, nums[j]);
+            }
+            Console.WriteLine("");
+            int counter = 0;
+            foreach (int k in nums)
+            {
+                Console.WriteLine("Element{0} = {1}", counter++, k);
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("");
+            // Create an array with the names of five friends
+            // greet all of them with a foreach loop
+
+            string[] myFriends = { "Michael", "Wlad", "Ilja", "Andy", "Daniel" };
+
+            foreach (string name in myFriends)
+            {
+                Console.WriteLine("Hi there {0}, my friend", name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 28_ForeachSwitchChallenge/28_ForeachSwitchChallenge/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace _28_ForeachSwitchChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean valid = false;
+            string inputValueType;
+
+            Console.Write("Enter a value : ");
+            string inputValue = Console.ReadLine();
+
+            Console.WriteLine("Select the Data type to validate the input you have entered");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
+
+            Console.Write("Enter : ");
+            int inputType = Convert.ToInt32(Console.ReadLine());
+
+            switch (inputType)
+            {
+                case 1:
+                    // check for string
+                    valid = IsAllAlphabetic(inputValue);
+                    inputValueType = "String";
+                    break;
+                case 2:
+                    // check for string
+                    valid = IsAllNumeric(inputValue);
+                    inputValueType = "Integer";
+                    break;
+                case 3:
+                    // check for string
+                    valid = IsBool(inputValue);
+                    inputValueType = "Boolean";
+                    break;
+                default:
+                    inputValueType = "";
+                    break;
+            }
+
+            if(inputValueType != "")
+            {
+                if (valid)
+                {
+                    Console.WriteLine("It is a valid :" + inputValueType);
+                }
+                else
+                {
+                    Console.WriteLine("It is a invalid :" + inputValueType);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input Value Type selection error!");
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Method to check if the input string is a pure letter
+        /// </summary>
+        static bool IsAllAlphabetic(string value)
+        {
+            foreach(char c in value)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Method to check if the input string is a pure number
+        /// </summary>
+        static bool IsAllNumeric(string value)
+        {
+            foreach (char c in value)
+            {
+                if (!char.IsNumber(c))
+                {   
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Method to check if the input string is a boolean (typed as a string)
+        /// </summary>
+        static bool IsBool(string value)
+        {
+            if(value.ToLower() == "true" || value.ToLower() == "false")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}

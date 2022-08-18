efca6acea2d92e1120fd8f1db97a6fb735618921
Arda AY <arda.ay@outlook.com.tr>
2022-08-18 21:03:26 +0300

    Arraylists and Lists

---
+++ 37_Lists/37_Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _37_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a method that returns a list of even integer numbers between 100 and 170 (inclusive).
+            
+            Console.WriteLine("Hello World!");
+            List<int> iList = Solution();
+
+            foreach (int item in iList)
+            {
+                Console.Write(item  + " ");
+            }
+        }
+
+        public static List<int> Solution()
+        {
+            // TODO: write your solution here
+            List<int> newList = new List<int>();
+            for (var i = 100; i <= 170; i += 2)
+            {
+                newList.Add(i);
+            }
+            return newList;
+        }
+    }
+}

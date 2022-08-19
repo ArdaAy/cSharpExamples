20c05ea5045c52ef54a73cec433b5bfc5f60bf51
Arda AY <arda.ay@outlook.com.tr>
2022-08-20 00:06:48 +0300

    Stacks

--- 39_Dictionaries/39_Dictionaries/Program.cs
+++ 39_Dictionaries/39_Dictionaries/Program.cs
@@ -22,7 +22,7 @@ namespace _39_Dictionaries
                 new Employee("Security","Cengo",10,100),
                 new Employee("Runner","Lucy",12,50)
             };
-
+            Dictionary<int, string> dict = new Dictionary<int, string>();
             Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
 
             foreach(Employee emp in employees)
@@ -40,7 +40,23 @@ namespace _39_Dictionaries
             {
                 Console.WriteLine("The key {0} does not exists", key);
             }
+
+            // Updating an item in Dictionary
+            string KeyToUpdate = "Runner";
+            if (employeesDirectory.ContainsKey(KeyToUpdate))
+            {
+                employeesDirectory[KeyToUpdate] = new Employee("HR", "LucyMucy", 13, 60);            
+            }
+
+            string KeyToDelete = "Security";
+            if (employeesDirectory.ContainsKey(KeyToUpdate))
+            {
+                employeesDirectory.Remove(KeyToDelete);
+            }
+
+            
             Console.WriteLine("");
+            
             for (int i = 0; i < employeesDirectory.Count; i++)
             {
                 // using ElementAt(i) to returnt the key-value pair stored at index i
@@ -59,6 +75,58 @@ namespace _39_Dictionaries
                 Console.WriteLine("Employee Salary: {0}", employeeValue.Salary);
                 Console.WriteLine("");
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("YOU CAN'T DELETE CENGO! HE IS MY BOY!!");
+
+            Console.WriteLine("");
+            employeesDirectory.Add("Security Master", new Employee("Security", "Cengo", 10, 100));
+
+            for (int i = 0; i < employeesDirectory.Count; i++)
+            {
+                // using ElementAt(i) to returnt the key-value pair stored at index i
+                KeyValuePair<string, Employee> keyValuePair = employeesDirectory.ElementAt(i);
+
+                // print the key
+                Console.WriteLine("Key: {0}, i is {1}", keyValuePair.Key, i);
+
+                // storing the value in an employee object
+                Employee employeeValue = keyValuePair.Value;
+
+                // printring the properties of the employee object
+                Console.WriteLine("Employee Name: {0}", employeeValue.Name);
+                Console.WriteLine("Employee Role: {0}", employeeValue.Role);
+                Console.WriteLine("Employee Age: {0}", employeeValue.Age);
+                Console.WriteLine("Employee Salary: {0}", employeeValue.Salary);
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            string var1 = Convert(2);
+            string var2 = Convert(6);
+            Console.WriteLine(var1);
+            Console.WriteLine(var2);
+        }
+
+        public static string Convert(int i)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(0, "zero");
+            dict.Add(1, "one");
+            dict.Add(2, "two");
+            dict.Add(3, "three");
+            dict.Add(4, "four");
+            dict.Add(5, "five");
+            if (dict.ContainsKey(i))
+            {
+                return dict[i];
+            }
+            else
+            {
+                return "nope";
+            }
+
 
         }
     }
---
+++ 40_Stacks/40_Stacks/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _40_Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //defining a new stack
+            Stack<int> stack = new Stack<int>();
+
+            //add elements to the stack using Push()
+            stack.Push(1);
+
+            //Peek() will return the element at the top of the stack without removing it
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+            stack.Push(2);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+            stack.Push(3);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+            Console.WriteLine("Popped item is: {0}", stack.Pop());
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Popped item is: {0}", stack.Pop());
+                Console.WriteLine("Stack Count: {0}", stack.Count);
+                
+            }
+
+            int[] numbers = new int[] { 8, 2, 3, 4, 7, 6, 2 };
+
+            Stack<int> myStack = new Stack<int>();
+
+            Console.WriteLine("the numbers in the array are:");
+            //foreach number in our array
+            foreach (int number in numbers)
+            {
+                //printing
+                Console.Write(number + " ");
+                myStack.Push(number);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("the numbers in reverse :");
+            //as long as our stack is not empty
+            while(myStack.Count > 0)
+            {
+                //Pop it and store it in a variable
+                int number = myStack.Pop();
+                //print the value we popped
+                Console.Write(number + " ");
+            }
+        }
+    }
+}

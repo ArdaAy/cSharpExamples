bf1ac65625936dfd6f531b65c17139a213be0bf2
Arda AY <arda.ay@outlook.com.tr>
2022-08-23 01:03:51 +0300

    New Inheritance Example

---
+++ 44_InheritanceDemo/44_InheritanceDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _44_InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Cengiz");
+            Console.WriteLine(post1.ToString());
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Cengiz",
+                "https://images.com/shoes", true);
+
+            Console.WriteLine(imagePost1.ToString());
+        }
+    }
+}

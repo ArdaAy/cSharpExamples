e0376c35c43a24ed3fe83fc064387a74805c77bb
Arda AY <arda.ay@outlook.com.tr>
2022-08-23 00:40:01 +0300

    Virtual and Override Methods Example

---
+++ 43_VirtualOverrideDemo/43_VirtualOverrideDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _43_VirtualOverrideDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Cabbar", 10);
+            Console.WriteLine($"{dog.Name} is {dog.Age} years old");
+            dog.MakeSound();
+            dog.Play();
+            dog.Eat();
+        }
+    }
+}

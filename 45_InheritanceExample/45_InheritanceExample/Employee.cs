e5abf2430b49f4a3fdcaf85ed82f895bad67b752
Arda AY <arda.ay@outlook.com.tr>
2022-08-23 21:42:42 +0300

    Another Inheritance Example

---
+++ 45_InheritanceExample/45_InheritanceExample/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45_InheritanceExample
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, string firstName, int salary)
+        {
+            this.Name = name;
+            this.FirstName = firstName;
+            this.Salary = salary;
+        }
+
+        public Employee()
+        {
+            Name = "Patato";
+            FirstName = "Cengiz";
+            Salary = 60000;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("I'm working");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("I'm having a break");
+        }
+    }
+}
---
+++ 45_InheritanceExample/45_InheritanceExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _45_InheritanceExample
+{
+    class Program
+    {
+        // Create a main class with the Main method, then a base class employee with the properties Name, 
+        // FirstName, Salary and the methods Work() and Pause().
+        // Create a deriving class boss with the property CompanyCar and the method Lead().
+        // Create another deriving class of employees - trainees with the properties WorkingHours 
+        // and SchoolHours and a method Learn();
+        // Override the methods Work() of the trainee class so that it indicates 
+        // the working hours of the trainee.
+        // Don't forget to create the constructors.
+        // Create an object of each of the three classes (with arbitrary values) 
+        // and call the methods, Lead() of Boss and Work() of Trainee.
+        // Just print out the respective text, what the respective employees do.
+
+        static void Main(string[] args)
+        {
+            Employee cengiz = new Employee("Cengiz", "Patato", 50000);
+
+            cengiz.Work();
+            cengiz.Pause();
+
+            Boss talakiFenden = new Boss("Ferrari", "Talaki", "Fenden", 999999999);
+
+            talakiFenden.Lead();
+
+            Trainee ciguli = new Trainee(32, 8, "Ciguli", "Miguel", 10000);
+            ciguli.Learn();
+            ciguli.Work();
+            Console.ReadKey();
+        }
+    }
+}

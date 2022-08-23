139145def61dabe122b20e3f249f260b991f44f3
Arda AY <arda.ay@outlook.com.tr>
2022-08-23 21:55:27 +0300

    Interface Intro Example

---
+++ 46_InterfacesIntro/46_InterfacesIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _46_InterfacesIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+
+            Console.WriteLine(t2.Equals(t1));
+        }
+    }
+}
---
+++ 46_InterfacesIntro/46_InterfacesIntro/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_InterfacesIntro
+{
+    class Ticket : IEquatable<Ticket>
+    {
+        // property to store the duration of the ticket in hours
+        public int DurationInHours { get; set; }
+
+        // simple construstor
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}

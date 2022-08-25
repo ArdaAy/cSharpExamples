cca1f4c1921938f245acc359e1613f504f793482
Arda AY <arda.ay@outlook.com.tr>
2022-08-26 01:19:44 +0300

    Abstract
    Also "as" and "is" keywords are used in this example

---
+++ 52_Abstract/52_Abstract/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _52_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Cube cube = new Cube(15);
+            cube.GetInfo();
+
+            Sphere sphere = new Sphere(15);
+            sphere.GetInfo();
+            */
+
+            Shape[] shapes = { new Sphere(4), new Cube(3) };
+
+            foreach (Shape shape in shapes)
+            {
+                shape.GetInfo();
+
+                Cube iceCube = shape as Cube;
+                if(iceCube == null)
+                {
+                    Console.WriteLine("This shape is not a cube");
+                }
+                
+                if(iceCube is Cube)
+                {
+                    Console.WriteLine("This is a cube!");
+                }
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+
+                Console.WriteLine("{0} Has a volume of {1}", cube2.Name, cube2.Volume());
+
+            }
+
+        }
+    }
+}

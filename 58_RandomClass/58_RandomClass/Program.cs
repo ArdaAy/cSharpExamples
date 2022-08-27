69e64b80991e21384928ce237dde8bf253b12402
Arda AY <arda.ay@outlook.com.tr>
2022-08-27 20:18:28 +0300

    Random class example

---
+++ 58_RandomClass/58_RandomClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _58_RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Random dice = new Random();
+            int numEyes;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine(numEyes);
+            }
+            */
+
+            Random yesNoMaybe = new Random();
+            int answerNum = yesNoMaybe.Next(1, 4);
+
+            if(answerNum == 1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if(answerNum == 2)
+            {
+                Console.WriteLine("Maybe");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}

17628c23d294cf65a0de9356f985d9155f8e0449
Arda AY <arda.ay@outlook.com.tr>
2022-08-27 17:42:58 +0300

    Math Class Example and Parsing Game Challenge

---
+++ 56_MathClass/56_MathClass/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _56_MathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ceiling 3.25 : " + Math.Ceiling(3.25));
+            Console.WriteLine("Flooring 3.25 : " + Math.Floor(3.25));
+
+            int num1 = 13;
+            int num2 = 9;
+
+            Console.WriteLine("Lower of num1 {0} and num2 {1} is {2}",num1,num2,Math.Min(num1,num2));
+            Console.WriteLine("Higher of num1 {0} and num2 {1} is {2}", num1, num2, Math.Max(num1, num2));
+
+            Console.WriteLine("3 to the power of 5 is {0}",Math.Pow(3,5));
+            Console.WriteLine("PI is {0}",Math.PI);
+
+            Console.WriteLine("The square root of 25 is {0}",Math.Sqrt(25));
+
+            Console.WriteLine("Always positive is {0}",Math.Abs(-25));
+
+            Console.WriteLine("Cos of 1 is {0}",Math.Cos(1));
+
+            
+            Run("20");
+            Run("asd");
+            Run("-1");
+
+        }
+
+        public static void Run(string line)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Given line is : {0} (range between 0 and 180)", line);
+            //string input = Console.ReadLine();
+            double parsedVal = 0;
+            bool isParsed = double.TryParse(line, out parsedVal);
+            if (isParsed)
+            {
+                if (parsedVal > 0 && parsedVal < 180)
+                {
+                    Console.WriteLine("Cos = {0}", Math.Cos(ConvertToRadians(parsedVal)));
+                    Console.WriteLine("Sin = {0}", Math.Sin(ConvertToRadians(parsedVal)));
+                    Console.WriteLine("Tg = {0}", Math.Tan(ConvertToRadians(parsedVal)));
+                }
+                else
+                {
+                    Console.WriteLine("Check the input!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Check the input!");
+            }
+
+        }
+
+        public static double ConvertToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+    }
+}
---
+++ 57_ParsingGame/57_ParsingGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _57_ParsingGame
+{
+    class Program
+    {
+        /*
+         * The rules:
+
+            Create a class Program with the Main method with code that will read from the input.txt line by line;
+
+            If one of the lines contains "split", you have to split it (use the split method from string class)
+
+            Take the return from the split and write the element with index 4 into the output.txt ;
+
+            You have to add a space between each element to make it readable;
+        */
+        static void Main(string[] args)
+        {
+            //C:\Users\ardaa\Desktop\C#\input.txt
+            string[] lines = File.ReadAllLines(@"C:\Users\ardaa\Desktop\C#\input.txt");
+
+            List<string> linesToWrite = new List<string>();
+            foreach (string line in lines)
+            {
+                if (line.Contains("split"))
+                {
+                    string[] splitedLine = line.Split();
+                    
+                    linesToWrite.Add(splitedLine[4]);
+                    
+                    
+                }
+            }
+
+            foreach (string item in linesToWrite)
+            {
+                Console.Write(item+" ");
+            }
+            
+            using (StreamWriter file = new StreamWriter(@"C:\Users\ardaa\Desktop\C#\output.txt"))
+            {
+                foreach (string item in linesToWrite)
+                {
+                    file.Write(item +" ");
+                }
+            }
+        }
+    }
+}

75863bec80a306b3de3896ef47d6cf4b851a0aa8
Arda AY <arda.ay@outlook.com.tr>
2022-08-29 23:20:28 +0300

    Delegates

---
+++ 63_DelegatesBasics/63_DelegatesBasics/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _63_DelegatesBasics
+{
+    class Program
+    {
+        // defining a delegate type called FilterDelegate that takes a
+        // person object and returns a bool
+        public delegate bool FilterDelegate(Person p);
+
+        static void Main(string[] args)
+        {
+            // Example 1
+            /*
+            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+
+            Console.WriteLine("-----before-----");
+            //print the names before the remove all method
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // calling RemoveAll and passing a method filter we created
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("-----after-----");
+            // print the names after the remove all method
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            */
+
+            // Example 2
+            // Create 4 person objects
+            Person p1 = new Person() { Name = "Aiden", Age = 41 };
+            Person p2 = new Person() { Name = "Sif", Age = 69 };
+            Person p3 = new Person() { Name = "Walter", Age = 12 };
+            Person p4 = new Person() { Name = "Anatoli", Age = 25 };
+
+            // add the objects to a list called people
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsMinor);
+            Console.WriteLine();
+            DisplayPeople("Adults", people, IsAdult);
+            Console.WriteLine();
+            DisplayPeople("Seniors", people, IsSenior);
+
+
+            FilterDelegate filter = delegate (Person p)
+            {
+                return p.Age >= 20 && p.Age <= 30;
+            };
+
+            Console.WriteLine();
+            DisplayPeople("Kids", people, filter);
+
+            Console.WriteLine();
+            foreach (Person p in people)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine(p.Name);
+                }
+            }
+
+            Console.WriteLine();
+
+            // Anonymous Method Usage
+            DisplayPeople("All: ", people, delegate (Person p) { return true; });
+
+
+        }
+
+        // a method to display the list of people that passes the filter condition (returns true)
+        // this method will take a little to be displayed, the list of people, and a filter delegate
+        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            // print the title
+            Console.WriteLine(title);
+
+            foreach (Person p in people)
+            {
+                // check if this person passes the filter
+                if (filter(p))
+                {
+                    // print the person's name and age
+                    Console.WriteLine("{0}, {1} years old", p.Name, p.Age);
+                }
+            }
+        }
+
+        static bool Filter(string s)
+        {
+            // return whether the string s contains the letter 'i'
+            // the Contains method will return a bool which we will return as well
+            return s.Contains("i");
+        }
+
+        //==========FILTERS================
+
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
+
+        //=================================
+    }
+}
---
+++ 64_DelegatesExample/64_DelegatesExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _64_DelegatesExample
+{
+    /*
+     * Create a public delegate that will be called OperationDelegate and will get two arguments with float type and return float as well;
+     * 
+     * Create 4 static public methods called Add, Subtract, Multiply, and Divide. 
+     * All of them should get 2 arguments and return a float that corresponds to the result of the performed operation;
+     * 
+     * Create ApplyOperation method that gets 2 floats and one delegate and applies the operation to those values;
+     */
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            ApplyOperation(10, 5, Add);
+            ApplyOperation(10, 5, Subtract);
+            ApplyOperation(10, 5, Multiply);
+            ApplyOperation(10, 5, Divide);
+            */
+            Console.WriteLine(ApplyOperation(10, 5, Add));
+            Console.WriteLine(ApplyOperation(10, 5, Subtract));
+            Console.WriteLine(ApplyOperation(10, 5, Multiply));
+            Console.WriteLine(ApplyOperation(10, 5, Divide));
+        }
+
+        public static float ApplyOperation(float num1, float num2, OperationDelegate od)
+        {
+            return od(num1, num2);
+        }
+
+        /*
+        public static void ApplyOperation(float num1, float num2, OperationDelegate od)
+        {
+            Console.WriteLine(od(num1, num2));
+        }
+        */
+        public delegate float OperationDelegate(float num1, float num2);
+
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public static float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}

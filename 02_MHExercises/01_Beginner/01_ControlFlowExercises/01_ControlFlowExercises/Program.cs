8f8cae4852cc9d8534c8629cde838132b4e0b1ee
Arda AY <arda.ay@outlook.com.tr>
2023-08-20 19:13:27 +0300

    Control Flow Exercises Completed

--- 02_MHExercises/01_Beginner/01_ControlFlowExercises/01_ControlFlowExercises/Program.cs
+++ 02_MHExercises/01_Beginner/01_ControlFlowExercises/01_ControlFlowExercises/Program.cs
@@ -10,6 +10,11 @@ namespace _01_ControlFlowExercises
             //Exercise02();
             //Exercise03();
             //Exercise04();
+            //Exercise05();
+            //Exercise06();
+            //Exercise07();
+            //Exercise08();
+            Exercise09();
         }
 
         /* Exercise01
@@ -101,6 +106,113 @@ namespace _01_ControlFlowExercises
                 else
                     Console.WriteLine("Demerit Point is {0}", demeritPoint);
             }
+        }
+
+        /* Exercise05
+         * Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+         * Display the count on the console.
+         */
+        static void Exercise05()
+        {
+            int cnt = 0;
+            int maxNumber = 100;
+            for (int i = 1; i < maxNumber; i++)
+            {
+                if (i % 3 == 0)
+                    cnt++;
+            }
+            Console.WriteLine("The count of the numbers between 1 and 100 are divisible by 3 with no remainder is {0}",cnt);
+        }
+
+        /* Exercise06
+         * Write a program and continuously ask the user to enter a number or "ok" to exit. 
+         * Calculate the sum of all the previously entered numbers and display it on the console.
+         */
+        static void Exercise06()
+        {
+            var total = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number to sum (Enter 'ok' if you are done)");
+                var enteredValue = Console.ReadLine();
+                if (enteredValue == "ok")
+                    break;
+                else
+                    total += int.Parse(enteredValue);
+            }
+            Console.WriteLine("Sum of the given numbers is {0}",total);
+        }
+
+        /* Exercise07
+         * Write a program and ask the user to enter a number. 
+         * Compute the factorial of the number and print it on the console. 
+         * For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+         */
+        static void Exercise07()
+        {
+            Console.WriteLine("Enter a number to take factorial value");
+            var number = int.Parse(Console.ReadLine());
+            var total = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                total *= i;
+            }
+            Console.WriteLine("Calculated facrotial number is {0}",total);
+        }
+
+        /* Exercise08
+         * Write a program that picks a random number between 1 and 10. 
+         * Give the user 4 chances to guess the number. 
+         * If the user guesses the number, display “You won"; otherwise, display “You lost". 
+         * (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+         */
+        static void Exercise08()
+        {
+            var randomNumber = new Random().Next(1,10);
+            Console.WriteLine("Number is {0}",randomNumber);
+            var cnt = 0;
+            while(cnt < 4)
+            {
+                Console.WriteLine("Make a guess");
+                var guess = int.Parse(Console.ReadLine());
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("You Won!");
+                    break;
+                }
+                else
+                    Console.WriteLine("Try Again");
+                cnt++;
+                if(cnt == 4)
+                    Console.WriteLine("You lost");
+            }
+        }
+
+        /* Exercise09
+         * Write a program and ask the user to enter a series of numbers separated by comma. 
+         * Find the maximum of the numbers and display it on the console. 
+         * For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+         */
+        static void Exercise09()
+        {
+            Console.WriteLine("Enter series of numbers separated by comma (For Example '1,2,3,4')");
+            var numberSeries = Console.ReadLine();
+            var splitedStr =  numberSeries.Split(",");
+            var numbers = Array.ConvertAll(splitedStr, int.Parse);
+
+            if(numbers.Length > 0)
+            {
+                int selectedNumber = numbers[0];
+                for(var i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > selectedNumber)
+                        selectedNumber = numbers[i];
+                }
+
+                Console.WriteLine("Max number in the series is {0}", selectedNumber);
+            }
+            else
+                Console.WriteLine("Please enter more than one number");
         }
     }
 }

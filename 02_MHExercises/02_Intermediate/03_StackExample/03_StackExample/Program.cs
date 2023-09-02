b22b65c4aad5b213bfad8a367cdf74bc6de01e88
Arda AY <arda.ay@outlook.com.tr>
2023-09-02 22:51:22 +0300

    Stack Exercise is completed

---
+++ 02_MHExercises/02_Intermediate/03_StackExample/03_StackExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _03_StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            bool IsProgramWorking = true;
+
+            while (IsProgramWorking)
+            {
+                ActionPrinter();
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        AddStackItem(stack);
+                        break;
+                    case "2":
+                        stack.Pop();
+                        break;
+                    case "3":
+                        stack.PrintStack();
+                        break;
+                    case "4":
+                        stack.Clear();
+                        break;
+                    case "5":
+                        Console.WriteLine("Quiting...");
+                        IsProgramWorking = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void AddStackItem(Stack stack)
+        {
+            Console.WriteLine("Give the value that you want to add to stack");
+            var val = Console.ReadLine();
+            stack.Push(val);
+        }
+
+
+        public static void ActionPrinter()
+        {
+            Console.WriteLine("Select Action Below");
+            Console.WriteLine("1-Push item");
+            Console.WriteLine("2-Pop item");
+            Console.WriteLine("3-Print stack");
+            Console.WriteLine("4-Clear stack");
+            Console.WriteLine("5-Quit");
+        }
+    }
+}
---
+++ 02_MHExercises/02_Intermediate/03_StackExample/03_StackExample/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_StackExample
+{
+    class Stack
+    {
+        private List<object> _stack;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void PrintStack()
+        {
+            if(_stack.Count == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("\nStack Printing");
+            for(var i = _stack.Count - 1;i >= 0; i--)
+            {
+                Console.WriteLine(_stack[i]);
+            }
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+
+        public void Push(object obj)
+        {
+            _stack.Add(obj);
+        }
+
+        public void Pop()
+        {
+            Console.WriteLine(_stack[_stack.Count - 1]);
+            _stack.RemoveAt(_stack.Count - 1);
+        }
+    }
+}

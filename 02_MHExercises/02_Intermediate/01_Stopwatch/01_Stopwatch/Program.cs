a78eda0359b26349dbd44ae44ed65cfd9f09aac9
Arda AY <arda.ay@outlook.com.tr>
2023-09-02 17:55:50 +0300

    Stopwatch Example is completed

---
+++ 02_MHExercises/02_Intermediate/01_Stopwatch/01_Stopwatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _01_Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Timer = new Stopwatch();
+            var ProgramRunning = true;
+            while (ProgramRunning)
+            {
+                ActionPrinter();
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Timer.Start();
+                        break;
+                    case "2":
+                        Timer.End();
+                        break;
+                    case "3":
+                        Timer.GetTimespan();
+                        break;
+                    case "4":
+                        Timer.ResetTime(1);
+                        break;
+                    case "5":
+                        Console.WriteLine("Program has been ended. Goodbye.");
+                        ProgramRunning = false;
+                        break;
+                    default: 
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void ActionPrinter()
+        {
+            Console.WriteLine("Select Actions below");
+            Console.WriteLine("1-Start Timer");
+            Console.WriteLine("2-End Timer");
+            Console.WriteLine("3-Show Timespan");
+            Console.WriteLine("4-Reset");
+            Console.WriteLine("5-Quit");
+        }
+    }
+}
---
+++ 02_MHExercises/02_Intermediate/01_Stopwatch/01_Stopwatch/Stopwatch.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Stopwatch
+{
+    class Stopwatch
+    {
+        private DateTime _start;
+        private DateTime _end;
+        private TimeSpan _timeSpan;
+        private bool _timeStarted;
+        private bool _timeEnded;
+        
+
+        public Stopwatch()
+        {
+            ResetTime(0);
+        }
+
+        public void Start()
+        {
+            if(!_timeStarted)
+            {
+                ResetTime(0);
+                StartTime();
+                Console.WriteLine("New time is started");
+            }
+            else
+                Console.WriteLine("Timer is started already. You need to reset to start new timer again.");
+            
+        }
+
+        public void End()
+        {
+            if (!_timeEnded && _timeStarted)
+            {
+                EndTime();
+                _timeSpan = _end - _start;
+                Console.WriteLine("Time is stopped succesfully. You can take timespan value now.");
+            }
+            else if(!_timeStarted)
+                Console.WriteLine("Timer is not started, you need to start it first.");
+            else if(_timeEnded)
+                Console.WriteLine("Timer is ended, You need to reset to start and stop new timer again");
+        }
+
+        public void GetTimespan()
+        {
+            if(_timeStarted && _timeEnded)
+            {
+                Console.WriteLine("Start Time: {0}\nEnd Time: {1}", _start, _end);
+                Console.WriteLine("Total duration is {0}:{1}", _timeSpan.Minutes,_timeSpan.Seconds);
+                Console.WriteLine("You can reset the system if you want to start and stop new timer.");
+            }
+            else if(!_timeStarted)
+                Console.WriteLine("Timer is not started. You need to start it first.");
+            else if(!_timeEnded)
+                Console.WriteLine("Timer is not ended. You need to end it first");
+        }
+
+        public void ResetTime(int state)
+        {
+            _start = new DateTime();
+            _end = new DateTime();
+            _timeSpan = new TimeSpan();
+            _timeStarted = false;
+            _timeEnded = false;
+            if(state == 1)
+                Console.WriteLine("Time is reset.");
+        }
+
+        private void StartTime()
+        {
+            _start = DateTime.Now;
+            _timeStarted = true;
+        }
+
+        private void EndTime()
+        {
+            _end = DateTime.Now;
+            _timeEnded = true;
+            //_timeStarted = false;
+        }
+
+        
+
+
+    }
+}
